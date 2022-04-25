3f6e35e1fb52ceeae4ce0efb14e25181dd1c8edd
Sofia Miikhaylenko <mikhaylenko-sd@yandex.ru>
2022-04-26 00:13:08 +0300

    homework sprint2 with fixes

--- src/Main.java
+++ src/Main.java
@@ -19,20 +19,28 @@ public class Main {
 
         while (true) {
             printMenu();
-            int choice = sc.nextInt();
+            String choice = sc.nextLine();
 
-            if (choice == 1) {
-                for (int i = 0; i < NUM_OF_REPORT_MONTH; i++) {
-                    fileContents = readFileContentsOrNull("resources\\m." + NUM_OF_YEAR + "0" + (i + 1) + ".csv");
-                    MonthlyReport monthlyReport = new MonthlyReport(i + 1, fileContents);
-                    monthlyReports.add(monthlyReport);
-                    System.out.println();
+            if (choice.equals("1")) {
+                if (monthlyReports.isEmpty()) {
+                    for (int i = 0; i < NUM_OF_REPORT_MONTH; i++) {
+                        fileContents = readFileContentsOrNull("resources\\m." + NUM_OF_YEAR + "0" + (i + 1) + ".csv");
+                        MonthlyReport monthlyReport = new MonthlyReport(i + 1, fileContents);
+                        monthlyReports.add(monthlyReport);
+                    }
+                } else {
+                    System.out.println("Месячные отчеты были считаны ранее.");
                 }
-            } else if (choice == 2) {
-                fileContents = readFileContentsOrNull("resources\\y." + NUM_OF_YEAR + ".csv");
-                yearlyReport = new YearlyReport(NUM_OF_YEAR, fileContents);
-                System.out.println();
-            } else if (choice == 3) {
+
+            } else if (choice.equals("2")) {
+                if (yearlyReport == null) {
+                    fileContents = readFileContentsOrNull("resources\\y." + NUM_OF_YEAR + ".csv");
+                    yearlyReport = new YearlyReport(NUM_OF_YEAR, fileContents);
+                } else {
+                    System.out.println("Годовой отчет был считан ранее.");
+                }
+            } else if (choice.equals("3")) {
+                boolean isCorrect = true;
                 if (monthlyReports.size() != 0 && yearlyReport != null) {
                     for (MonthlyReport monthlyReport : monthlyReports) {
                         int sumExpense = 0;
@@ -46,16 +54,22 @@ public class Main {
                         }
                         YearReportItem yearlyReportByMonthExpense = yearlyReport.getByMonthExpense(monthlyReport.getMonthNum(), true);
                         YearReportItem yearlyReportByMonthNotExpense = yearlyReport.getByMonthExpense(monthlyReport.getMonthNum(), false);
-                        if (yearlyReportByMonthExpense.getAmount() == sumExpense && yearlyReportByMonthNotExpense.getAmount() == sumNotExpense) {
-                            System.out.println("Сверка за " + getMonthName(monthlyReport.getMonthNum()) + " прошла успешно.");
-                        } else {
-                            System.out.println("При сверке обнаружено несоответсвие в месяце " + monthlyReport.getMonthNum());
+                        if (yearlyReportByMonthExpense.getAmount() != sumExpense) {
+                            isCorrect = false;
+                            System.out.println("При сверке расходов обнаружено несоответсвие в месяце " + yearlyReportByMonthExpense.getMonth());
                         }
+                        if (yearlyReportByMonthNotExpense.getAmount() != sumNotExpense) {
+                            isCorrect = false;
+                            System.out.println("При сверке доходов обнаружено несоответсвие в месяце " + yearlyReportByMonthExpense.getMonth());
+                        }
+                    }
+                    if (isCorrect) {
+                        System.out.println("Сверка прошла успешно");
                     }
                 } else {
                     System.out.println("Вы не выполнили команды - 1 и/или 2. Считайте все отчеты и повторите попытку.");
                 }
-            } else if (choice == 4) {
+            } else if (choice.equals("4")) {
                 if (monthlyReports.size() != 0) {
                     for (MonthlyReport monthlyReport : monthlyReports) {
                         System.out.println(getMonthName(monthlyReport.getMonthNum()));
@@ -64,12 +78,12 @@ public class Main {
                         System.out.println("Самая большая трата: " + maxExpenseItem.getItemName() + " - "
                                 + maxExpenseItem.getQuantity() * maxExpenseItem.getSumOfOne());
                         System.out.println("Самый прибыльный товар: " + maxNotExpenseItem.getItemName() + " - "
-                                + maxExpenseItem.getQuantity() * maxExpenseItem.getSumOfOne());
+                                + maxNotExpenseItem.getQuantity() * maxNotExpenseItem.getSumOfOne());
                     }
                 } else {
                     System.out.println("Вы не выполнили команду - 1. Считайте месячные отчеты и повторите попытку.");
                 }
-            } else if (choice == 5) {
+            } else if (choice.equals("5")) {
                 if (yearlyReport != null) {
                     System.out.println(yearlyReport.getYearNum() + " год");
                     for (int i = 0; i < NUM_OF_REPORT_MONTH; i++) {
@@ -79,7 +93,7 @@ public class Main {
                 } else {
                     System.out.println("Вы не выполнили команду - 2. Считайте годовой отчет и повторите попытку.");
                 }
-            } else if (choice == 0) {
+            } else if (choice.equals("exit")) {
                 System.out.println("Выход из приложения.");
                 break;
             } else {
@@ -96,7 +110,7 @@ public class Main {
         System.out.println("3 - Сверить отчёты.");
         System.out.println("4 - Вывести информацию о всех месячных отчётах.");
         System.out.println("5 - Вывести информацию о годовом отчёте.");
-        System.out.println("0 - Выйти из приложения");
+        System.out.println("exit - Выйти из приложения");
     }
 
     public static String getMonthName(int monthNum) {
@@ -114,7 +128,7 @@ public class Main {
         try {
             return Files.readString(Path.of(path));
         } catch (IOException e) {
-            System.out.println("Невозможно прочитать файл с месячным отчётом. Возможно, файл не находится в нужной директории.");
+            System.out.println("Невозможно прочитать файл: " + path + ". Возможно, файл не находится в нужной директории.");
             return null;
         }
     }
